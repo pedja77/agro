d2433847a0dee1aedcd0459be9a59fc985f9eb31
pedja77 <pedja77@stcable.net>
2018-06-07 14:53:20 +0200

    Navbar and carousel mockup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('./layouts/master')
+
+@section('content')
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+        esse rerum earum atque molestiae ex reprehenderit. Laudantium sequi
+        unde accusantium optio earum nam reprehenderit, minus eius qui odit a libero!
+    </p>
+@endsection
